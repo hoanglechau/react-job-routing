1d36579ebc2e56ea529d0a92271fe70013b0f0fe
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-19 01:41:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import AuthProvider from './auth/AuthProvider';
 import ThemeProvider from './theme/ThemeProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
