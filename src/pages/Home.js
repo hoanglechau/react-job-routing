d803d47964e63784aa0d7e3cf2022b0b09c49801
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-19 01:41:55 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import Pagination from '@mui/material/Pagination';
 import { styled } from '@mui/material/styles';
+import { useSearchParams } from 'react-router-dom';
+import { Typography } from '@mui/material';
 import api from '../data/fetchData';
 import JobCard from '../components/JobCard';
 
@@ -15,41 +17,52 @@ const CenterPagination = styled(Pagination)(({ theme }) => ({
 
 function Home() {
   const [jobs, setJobs] = useState([]);
-  const [page, setPage] = useState(1);
   const [pagesTotal, setPagesTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
+  const q = searchParams.get('q');
   const handleChange = (event, value) => {
     setPage(value);
   };
 
   useEffect(() => {
     const fetch = async () => {
-      const data = await api.getJobs(page);
+      const data = await api.getJobs(page, q);
       setJobs(data.jobs);
       setPagesTotal(data.pagesTotal);
     };
     fetch();
-  }, [page]);
+  }, [page, q]);
 
   return (
     <Container sx={{ p: 3 }} maxWidth="lg">
-      <Grid container spacing={3}>
-        {jobs.map((job) => (
-          <Grid key={job.id} item md={4} sm={6} xs={12}>
-            <JobCard
-              id={job.id}
-              title={job.title}
-              description={job.description}
-              skills={job.skills}
-            />
+      {jobs.length > 0 ? (
+        <>
+          <Grid container spacing={3}>
+            {jobs.map((job) => (
+              <Grid key={job.id} item md={4} sm={6} xs={12}>
+                <JobCard
+                  id={job.id}
+                  title={job.title}
+                  description={job.description}
+                  skills={job.skills}
+                />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-      <CenterPagination
-        sx={{ marginTop: '15px' }}
-        count={pagesTotal}
-        color="error"
-        onChange={handleChange}
-      />
+          <CenterPagination
+            sx={{ marginTop: '15px' }}
+            count={pagesTotal}
+            color="error"
+            onChange={handleChange}
+          />
+        </>
+      ) : (
+        <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>
+          No Result
+        </Typography>
+      )}
     </Container>
   );
 }
