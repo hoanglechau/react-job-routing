578f566a6e05f42f57a13c3fb5067280f0613206
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-19 01:41:48 +0700

    Update fetchData.js

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -1,13 +1,37 @@
 import jobs from './jobs.json';
 
-async function getJobs(page) {
-  const promise = new Promise((resolve) => {
+async function getJobs(page, q = null) {
+  // eslint-disable-next-line no-unused-vars
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 100);
+  });
+
+  await promise;
+
+  if (q) {
+    const filterJobs = jobs.filter(
+      (job) => job.title.includes(q)
+            || job.description.includes(q)
+            || job.city.includes(q)
+            || job.skills.some((skill) => skill.includes(q)),
+    );
+    return { jobs: filterJobs, pagesTotal: 1 };
+  }
+  return { jobs: jobs.slice((page - 1) * 6, page * 6 - 1), pagesTotal: 3 };
+}
+
+async function getJob(id) {
+  // eslint-disable-next-line no-unused-vars
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve();
     }, 100);
   });
   await promise;
-  return { jobs: jobs.slice((page - 1) * 6, page * 6 - 1), pagesTotal: 3 };
+
+  return jobs.find((job) => job.id === id);
 }
 
-export default { getJobs };
+export default { getJobs, getJob };
