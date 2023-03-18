4afebd66894203b9869dbbac4f2946007b359507
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-19 05:34:32 +0700

    Update JobDetail.js

--- src/pages/JobDetail.js
+++ src/pages/JobDetail.js
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import RequireAuth from '../auth/RequireAuth';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import SkillsPaper from '../components/SkillsPaper';
 import api from '../data/fetchData';
+import RequireAuth from '../auth/RequireAuth';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: { xs: '90%', md: 600 },
+  bgcolor: 'background.paper',
+  borderRadius: 2,
+  border: 'none',
+};
 
 function JobDetail() {
   const [job, setJob] = useState();
   const { id } = useParams();
+  // const navigate = useNavigate();
+  // const from = navigate.state?.from?.pathname || '/';
+
   useEffect(() => {
     const fetch = async () => {
       const data = await api.getJob(id);
@@ -16,7 +35,48 @@ function JobDetail() {
   return (
     <div>
       <RequireAuth callback={() => {}}>
-        <h1>{job?.title}</h1>
+        <Box sx={style}>
+          <Card
+            sx={{
+              border: 'none',
+              boxShadow: 0,
+              backgroundColor: (theme) => theme.palette.primary.light,
+              color: (theme) => theme.palette.common.white,
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {job?.title}
+              </Typography>
+              <SkillsPaper skills={job?.skills} />
+              <Typography>{job?.description}</Typography>
+              <Typography variant="h6" component="div">
+                City:
+                {' '}
+                {job?.city}
+              </Typography>
+              <Typography variant="h6" component="div">
+                Salary Range:
+                {'  $'}
+                {job?.salaryLow.toLocaleString('en-US')}
+                {' '}
+                -
+                {'  $'}
+                {job?.salaryHigh.toLocaleString('en-US')}
+              </Typography>
+              <Typography variant="h6" component="div">
+                Years of Experience:
+                {' '}
+                {job?.yrsXPExpected}
+              </Typography>
+              <Typography variant="h6" component="div">
+                Remote:
+                {' '}
+                {job?.remote ? 'Yes' : 'No'}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
       </RequireAuth>
     </div>
   );
