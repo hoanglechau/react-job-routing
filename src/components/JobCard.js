cb6122ff385bc3ee4aca963a454434fc506fa693
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-21 03:50:36 +0700

    Update JobCard.js

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
@@ -8,7 +8,6 @@ import Divider from '@mui/material/Divider';
 import { Link, useLocation } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import SkillsPaper from './SkillsPaper';
-import AuthContext from '../auth/AuthContext';
 
 // Card wrapper
 const CardStyle = styled(Card)(({ theme }) => ({
@@ -25,8 +24,20 @@ const CardStyle = styled(Card)(({ theme }) => ({
 function JobCard({
   description, skills, id, title,
 }) {
-  const auth = useContext(AuthContext);
   const location = useLocation();
+  /*
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleClickMore = () => {
+    // eslint-disable-next-line react/destructuring-assignment
+    if (auth.user) {
+      navigate(`job/${id}`, { state: { backgroundLocation: location } });
+    } else {
+      navigate('/login', { state: { backgroundLocation: location } });
+    }
+  };
+  */
 
   return (
     <CardStyle ariant="outlined">
@@ -45,48 +56,23 @@ function JobCard({
           >
             {title}
           </Typography>
-          <Typography
-            variant="subtitle2"
-            component="div"
-            sx={{
-              margin: '0 auto 5px', borderRadius: 0.5, width: '50%', bgcolor: '#383030', textAlign: 'center', color: (theme) => theme.palette.common.white,
-            }}
-          >
-            Job ID:
-            {` ${id}`}
-          </Typography>
           <Divider />
           <SkillsPaper skills={skills} />
           <Typography sx={{ fontSize: '0.8em', color: (theme) => theme.palette.common.white }}>
             {description}
           </Typography>
         </CardContent>
-        {/* eslint-disable-next-line react/destructuring-assignment */}
-        {auth.user ? (
-          <Button
-            variant="contained"
-            component={Link}
-            to={`job/${id}`}
-            state={{ backgroundLocation: location }}
-            sx={{
-              width: '130px', backgroundColor: '#df9e0b', color: 'black', p: '2px',
-            }}
-          >
-            Learn More
-          </Button>
-        ) : (
-          <Button
-            variant="contained"
-            component={Link}
-            to="login"
-            state={{ backgroundLocation: location }}
-            sx={{
-              width: '130px', backgroundColor: '#df9e0b', color: 'black', p: '2px',
-            }}
-          >
-            Learn More
-          </Button>
-        )}
+        <Button
+          variant="contained"
+          component={Link}
+          to={`job/${id}`}
+          state={{ backgroundLocation: location }}
+          sx={{
+            width: '130px', backgroundColor: '#df9e0b', color: 'black', p: '2px',
+          }}
+        >
+          Learn More
+        </Button>
       </Stack>
     </CardStyle>
   );
